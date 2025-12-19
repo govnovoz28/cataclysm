faf5feebc6a310a4932ad4486ff091bd66f3aa26
govnovoz28 <svatoslavkopaev37@gmail.com>
2025-12-20 01:37:04 +0300

    Update actions.ts

--- app/actions.ts
+++ app/actions.ts
@@ -1,18 +1,21 @@
-// app/actions.ts
+
 'use server'
 
-// 👇 ИЗМЕНЕНИЕ: используем относительный путь (две точки в начале)
 import { createClient } from '../utils/supabase/server'; 
 import { cookies } from 'next/headers';
+import { revalidatePath } from 'next/cache';
 
 export async function incrementView(postId: string) {
   const cookieStore = await cookies();
-  const supabase = createClient(cookieStore);
 
-  // Вызов RPC функции базы данных
+  const supabase = await createClient(cookieStore);
   const { error } = await supabase.rpc('increment_view_count', { post_id: postId });
 
   if (error) {
     console.error('Error incrementing view:', error);
+    return;
   }
+
+  revalidatePath('/');
+  revalidatePath(`/post/${postId}`);
 }
