15a16d797dcee9d64362a41002713c725fc38384
govnovoz28 <svatoslavkopaev37@gmail.com>
2025-12-24 00:50:14 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -88,7 +88,7 @@ export default async function Home({
                 alt="Cataclysm Logo"
                 width={200} 
                 height={200} 
-                className="w-12 h-12 md:w-16 md:h-16 object-contain opacity-90"
+                className="w-11 h-11 md:w-15 md:h-15 object-contain opacity-90"
             />
         </div>
 
