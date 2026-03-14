f5552c454e489d0dc3507e357c9470c3f3f80bf2
govnovoz28 <svatoslavkopaev37@gmail.com>
2026-03-14 21:43:54 +0300

    update

--- next.config.ts
+++ next.config.ts
@@ -9,22 +9,12 @@ const nextConfig: NextConfig = {
   },
   images: {
     unoptimized: true,
-    remotePatterns:[
+    remotePatterns: [
       {
         protocol: 'https',
         hostname: 'kksblfpjhrkbuuvsbvcf.supabase.co',
       },
     ],
-  },
-  async redirects() {
-    return[
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'cataclysm.online' }],
-        destination: 'https://www.cataclysm.online/:path*',
-        permanent: true,
-      },
-    ];
   },
 };
 
